38ac514c05fa0447b945fac8558479a2f7690c3f
DURANDamaury <bdciron@gmail.com>
2022-12-29 15:18:03 +0100

    media query ajouté pour page index

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,18 @@
 import './Gallery.scss'
+import Thumb from './Thumb.jsx'
+
+import Datas from "../../datas/logements.json"
+
 
 function Gallery()
     {
         return (
             <section>
-
+                {
+                Datas.map((Logement,index) => (
+                    <Thumb value={`${Logement.id}`} />
+                ))
+                }
             </section>
         )
     }
---
+++ src/components/Gallery/Thumb.jsx
@@ -0,0 +1,20 @@
+import './Thumb.scss';
+import Datas from "../../datas/logements.json"
+
+function Thumb(props)
+    {
+        const value = props.value;
+        const key = value.toString();
+        const Logement = Datas.find(Logement => Logement.id === props.value)
+
+
+        return (
+            <div key={key} className="Thumb" style={{backgroundImage: `url(${Logement.cover})`}}>
+                <div className="Thumb_title">
+                    <div className="Thumb_title_txt">{Logement.title}</div>
+                </div>
+            </div>
+        )
+    }
+
+export default Thumb
