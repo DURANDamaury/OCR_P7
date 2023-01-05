f621b86ae9007201811d26edf8e00ac9dca8a709
DURANDamaury <bdciron@gmail.com>
2023-01-05 16:42:25 +0100

    ajout de la page 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Logement from './pages/Logement';
 import About from './pages/About';
+import P404 from './pages/404';
 //
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -14,7 +15,9 @@ function App()
             <Header/>
             <Routes>
                 <Route path="/APropos" element={<About/>} />
+                <Route path="/APropos/*" element={<P404/>} />
                 <Route path="/logement/:id" element={<Logement/>} />
+                <Route path="/logement/*" element={<P404/>} />
                 <Route path="/" element={<Home/>} />
                 <Route path="*" element={<Home/>} />        
             </Routes>
