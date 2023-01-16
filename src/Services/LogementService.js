2b1ffe0ee55c8bd4b553286dc86bb68797301c18
DURANDamaury <bdciron@gmail.com>
2023-01-16 17:24:45 +0100

    tentative de création d'un hook pour l'api (ne fonctionne pas)

--- src/Services/LogementService.js
+++ src/Services/LogementService.js
@@ -1,13 +1,24 @@
-function getAllLogements (Logements) 
-    {
-        const logementsList = JSON.Logements
-        return (logementsList)
-    }
+import { useState, useEffect } from 'react'
 
+//parameter api url to call
+export function useFetch(url) {            
 
-//function getLogementById (id) {
-//    return null;
+    const [data, setData] = useState({})            //internal state to stock data     
+    const [isLoading, setLoading] = useState(true)  //internal state for loading status(true for start)
 
-//}
+    useEffect(() => {
+        if (!url) return                            //if url is empty, return free
 
-export default getAllLogements
+        async function fetchData() {
+            const response = await fetch(url)       //fetch from url
+            const data = await response.json()      //response
+            setData(data)                           //data is set on state data
+            setLoading(false)                       //end of loading, status false
+            }
+
+        setLoading(true)                            //loading state is true before response is ended
+        fetchData()                                 //start fetchdata
+        }, [url])                                   //call when url change
+
+    return { isLoading, data }                      //return state isLoading and data
+}
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,34 +2,22 @@ import './Gallery.scss'
 import Thumb from './Thumb.jsx'
 import ThumbLoader from './ThumbLoader'
 
-import { useEffect , useState } from "react";
-//import getAllLogements from "../../Services/LogementService"
+import { useFetch } from '../../Services/LogementService';
+
 
 function Gallery()
     {
-        const [housingDatas,setHousingDatas] = useState([]);
-        const [isDataLoading, setDataLoading] = useState(false);
-
-        useEffect(() => {
-            setDataLoading(true)
-            fetch("./datas/logements.json")
-                .then((response) => response.json())
-                .then((Datas) =>    {
-                                    setHousingDatas(Datas)
-                                    setDataLoading(false)
-                                    })
-                .catch((error) => console.error(error));
-                }, []);
-        
-
+        const { data, isLoading } = useFetch(`./datas/logements.json`)
+        const [ housingData ] = data
 
         return (
             <section className='Gallery'>
-                {
-                housingDatas.map((housing) => (isDataLoading ? (
-                    <ThumbLoader />) : (<Thumb key={housing.id} id={housing.id} cover={housing.cover} title={housing.title} />)
-                ))
-                }
+                    {
+                    data.map((housing) => (isLoading ? (
+                        <ThumbLoader />) : (<Thumb key={housing.id} id={housing.id} cover={housing.cover} title={housing.title} />)
+                    ))
+                    }
+                
             </section>
         )
     }
