a0ae8d59e5abd7ecc221fab7b85468a5291308e4
DURANDamaury <bdciron@gmail.com>
2023-02-20 10:29:47 +0100

    modification faute accueil

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header()
             <header className="header">
                 <img className="header_logo" src={logo} alt="logo" />
                 <nav className="header_nav">
-                    <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>Acceuil</NavLink>
+                    <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>Accueil</NavLink>
                     <NavLink to="/APropos" className={(nav) => (nav.isActive ? "nav-active" : "")}>A Propos</NavLink>
                 </nav>
             </header>
